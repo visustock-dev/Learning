208074f29146dd212140a4975efdd3d95a3036e0
visustock-dev <dev@visustock.fr>
2014-06-09 17:32:27 -0500

    Added first program files

---
+++ FirstProgram2014/src/main.java
@@ -0,0 +1,10 @@
+
+public class main {
+	/**
+	 * This prints hello world
+	 * @System.out args
+	 */
+public static void main(String []args) {
+	System.out.println("Hello Word"); //prints hello world
+}
+}
